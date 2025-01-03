39f77daccc8dbb775da0106baccf2d0b41a83526
Barnold88 <bandslover@outlook.com>
2025-01-03 01:18:48 +0000

    Update fps controller to lerp for smoother movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,39 +1,49 @@
 extends CharacterBody3D
 
-
 @onready var head: Node3D = $Head
 
-
+## Const variables - Read only
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 0.4
 
-var current_speed = 5.0
+## Editable variables - Read and write
+var current_speed = 0.0
+var direction = Vector3(0.0,0.0,0.0)
+var head_height = 0.0
 
+
+## Exported variables for editing in godot editor
+@export var MAX_SPEED = 10.0
 @export var walking_speed = 5.0
 @export var sprinting_speed = 8.0
-@export var crouching_speed = 3.0
-
+@export var crouching_speed = 1.0
+@export var crouching_depth = 0.7
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Hide mouse
+	head_height = head.position.y
+	
 func _input(event: InputEvent) -> void:
-
-	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
-		head.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
-		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89),deg_to_rad(89))
+	if event is InputEventMouseMotion: # When the input event for a mouse movement is triggered...
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY)) # Rotate the y axis to look left and right ("-" sign to stop inversion)
+		head.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY)) # Rotate the x axis to look up and down ("-" sign to stop inversion )
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89),deg_to_rad(89)) # clamp to stop rolling effect when looking
 
 func _physics_process(delta: float) -> void:
-	
+
 	if Input.is_action_just_pressed("pause"):
 		get_tree().quit()
 
-	if Input.is_action_pressed("sprint"):
-		current_speed = sprinting_speed
+	if Input.is_action_pressed("crouch"):
+		head.position.y = crouching_depth
+		current_speed = crouching_speed
 	else:
-		current_speed = walking_speed
-		
+		if Input.is_action_pressed("sprint"):
+			current_speed = sprinting_speed
+		else:
+			current_speed = walking_speed
+		head.position.y = head_height
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -41,11 +51,11 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*MAX_SPEED)
 	
 	if direction:
 		velocity.x = direction.x * current_speed
