a4b3143040f13ae59493d429175e07fc35c22dc6
Barnold88 <bandslover@outlook.com>
2025-01-04 17:30:11 +0000

    Add functionality for interact prompt

--- Scripts/InteractorCast.gd
+++ Scripts/InteractorCast.gd
@@ -1,4 +1,5 @@
 extends RayCast3D
+@onready var interact_text: RichTextLabel = $"../../../../Control/InteractText"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +11,20 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	if is_colliding():
-		print(get_collider().name) # temp
+		
+		var collider = get_collider()
+		
+		match collider.name:
+			"ReadableBookCollider":
+				interact_text.visible = true
+				if Input.is_action_just_pressed("interact"):
+					print(collider.get_class())
+			"":
+				print("Two are better than one!")
+			_:
+				pass
+				
+	else:
+		interact_text.visible = false
 	
 	pass
